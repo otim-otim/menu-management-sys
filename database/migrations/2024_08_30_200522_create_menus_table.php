d5706fe27310555bfe2e7a8402b1888d5f48a321
Otim Isaac <isaacomega16@gmail.com>
2024-08-30 23:19:23 +0300

    add the menus table migration

---
+++ database/migrations/2024_08_30_200522_create_menus_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Name of the menu item
+            $table->unsignedBigInteger('parent_id')->nullable(); // Self-referencing foreign key
+            $table->integer('depth')->default(1); // Order of the menu item
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
