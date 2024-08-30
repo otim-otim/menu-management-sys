119e90be0a2bdfa81f7cef210833acf0ef02174d
Otim Isaac <isaacomega16@gmail.com>
2024-08-30 23:44:08 +0300

    modify api routes

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 
-Route::get('/menus', [MenuController::class, 'index'])->name('menus.index');
-Route::get('/menus/create', [MenuController::class, 'create'])->name('menus.create');
-Route::post('/menus', [MenuController::class, 'store'])->name('menus.store');
-Route::get('/menus/{menu}', [MenuController::class, 'show'])->name('menus.show');
-Route::get('/menus/{menu}/edit', [MenuController::class, 'edit'])->name('menus.edit');
-Route::put('/menus/{menu}', [MenuController::class, 'update'])->name('menus.update');
-Route::delete('/menus/{menu}', [MenuController::class, 'destroy'])->name('menus.destroy');
+
+Route::prefix('menus')->name('menus.')->controller(MenuController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{menu}', 'show')->name('show');
+    Route::get('/{menu}/edit', 'edit')->name('edit');
+    Route::put('/{menu}', 'update')->name('update');
+    Route::delete('/{menu}', 'destroy')->name('destroy');
+});
